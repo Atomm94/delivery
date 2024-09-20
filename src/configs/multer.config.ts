531db5094b46b3752d8f2ad27cd881d1022ee530
Atomm94 <atom.makaryan.94@gmail.com>
2024-09-20 04:38:14 +0400

    update

--- src/configs/multer.config.ts
+++ src/configs/multer.config.ts
@@ -2,7 +2,7 @@ import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { unlinkSync } from 'fs';
 
-const FILE_HOSTING_URL = process.env.HOSTING || 'http://localhost:3000';
+const FILE_HOSTING_URL = process.env.HOSTING || 'ec2-13-60-241-214.eu-north-1.compute.amazonaws.com';
 
 export const multerConfig = {
   storage: diskStorage({
