de33aeee9e4246dcc7e94a657efc8b218a991803
Atomm94 <atom.makaryan.94@gmail.com>
2024-11-10 23:14:31 +0400

    fix

--- src/route/route.module.ts
+++ src/route/route.module.ts
@@ -6,9 +6,10 @@ import { Customer } from '../database/entities/customer.entity';
 import { Route } from '../database/entities/route.entity';
 import { Order } from '../database/entities/order.entity';
 import { Product } from '../database/entities/product.entity';
+import { Address } from '../database/entities/address.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Route, Customer, Order, Product])],
+  imports: [TypeOrmModule.forFeature([Route, Customer, Order, Product, Address])],
   providers: [RouteService],
   controllers: [RouteController]
 })
