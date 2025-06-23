9c81b3ed94760e6a5f62f8f6fec3c9bc43ea8338
Atomm94 <atom.makaryan.94@gmail.com>
2025-06-24 01:24:36 +0400

    finally steps update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,33 +22,4 @@ export class AppController {
 
     return res.send(await this.appService.deleteUserData(user));
   }
-
-  @Get('/tt')
-  async ok(@Req() req, @Res() res) {
-    const socket = io('http://localhost:3000');
-
-// Prepare location data
-    const locationData = {
-      driverId: '12345',
-      location: {
-        lat: 40.7128,
-        lng: -74.0060
-      }
-    };
-
-// Emit a message to the server with the location data
-    socket.emit('message', locationData);
-
-// Listen for the server's response (optional)
-    socket.on('message', (data) => {
-      console.log('Received message from server:', data);
-    });
-
-// Listen for error responses
-    socket.on('error', (error) => {
-      console.error('Error from server:', error.message);
-    });
-
-    return res.send(socket.id);
-  }
 }
